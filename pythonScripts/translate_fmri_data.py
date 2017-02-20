0121bfe4abe3213aea69b2e98de38af742754442
andrewtran1995 <andrew.tran345@gmail.com>
2017-02-19 18:35:06 -0800

    Init translate_fmri_data.py and updated gitignore

---
+++ pythonScripts/translate_fmri_data.py
@@ -0,0 +1,8 @@
+# REQUIREMENTS
+# Python 2.7
+# Numpy 1.12.0
+
+# Following code is called if file is executed as such:
+# python translate_fmri_data.py data_directory
+if __name__ == '__main__':
+    pass
