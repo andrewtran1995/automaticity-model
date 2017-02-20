4bd37f1b658e0f2b4cd85fa2994e2dac52102628
andrewtran1995 <andrew.tran345@gmail.com>
2017-02-19 19:50:57 -0800

    Finished script

--- pythonScripts/translate_fmri_data.py
+++ pythonScripts/translate_fmri_data.py
@@ -15,7 +15,9 @@ REFERENCES
 # Import required Python modules
 import os
 import sys
+import shutil
 import re  # regular expressions
+import numpy as np  # really freakin' cool matrix features
 
 # Define constants
 DEFAULT_DST_DIR = os.path.join(os.path.dirname(__file__), "output")  # default destination is currentDirectory/output
@@ -39,6 +41,7 @@ def getDatFilesFromDirectory(dir_path):
     # Return files
     return files
 
+
 def standardize_name(filename):
     """
     Standardize a data file name such that subject number always takes up three characters, and underscores are used
@@ -49,6 +52,26 @@ def standardize_name(filename):
     subject_num = re.search(RE_SUBJECT_NUM, filename).group()
     trial_num = re.search(RE_SCAN_NUM, filename).group()
     return "sub_{:03d}_{}_data.dat".format(int(subject_num), trial_num)
+
+
+def flagged_name(standardized_name):
+    return "dup_" + standardized_name
+
+
+def load_dat_file(datname):
+    return np.genfromtxt(datname, dtype=np.float)
+
+
+def dat_files_identical(dat1, dat2):
+    """
+    Compares two dat files' contents and determines if they are equal.
+    Unlike regular integers, equality for floats can not be done exactly, and must be treated
+    with a level of error tolerance (i.e., "is it close enough?").
+    :param dat1:
+    :param dat2:
+    :return: boolean indicating whether dat1 is the same matrix as dat2
+    """
+    return np.allclose(load_dat_file(dat1), load_dat_file(dat2))
 
 
 # Following code is called if file is executed as such:
@@ -66,13 +89,31 @@ if __name__ == '__main__':
         dst_dir = sys.argv[2]
     else:
         dst_dir = DEFAULT_DST_DIR
-    # Define dictionary to map old file names/paths to new file names/paths
+    # Define dictionary to map new file names/paths to old file names/paths
+    # to keep track of what "new" filenames are already taken
     filenames = dict()
     # Get all valid files
     for filename in getDatFilesFromDirectory(src_dir):
         new_name = standardize_name(filename)
-        if new_name not in filenames.values():
-            filenames[filename] = new_name
+        # If new_name is not "taken" by another file, insert it into the dictionary and point to the old filename
+        if new_name not in filenames:
+            filenames[new_name] = filename
+        # Else, must resolve a name conflict between two files
         else:
-            # Resolve conflict
-
+            dat1 = filename
+            dat2 = filenames[new_name]
+            # If dat files are identical, the current filename is a duplicate of one we have already accounted for.
+            # Therefore, do not insert it into the filenames dictionary and continue the loop.
+            if dat_files_identical(dat1, dat2):
+                continue
+            # Otherwise, create a unique filename to indicate that this dat file is an anomaly.
+            else:
+                filenames[flagged_name(new_name)] = filename
+    # Create dst_dir if it does not already exist
+    if os.path.exists(dst_dir) is False:
+        os.makedirs(dst_dir)
+    # Create new files using dictionary
+    for new_name, old_name in filenames.items():
+        old_path = os.path.join(src_dir, old_name)
+        new_path = os.path.join(dst_dir, new_name)
+        shutil.copyfile(old_path, new_path)
