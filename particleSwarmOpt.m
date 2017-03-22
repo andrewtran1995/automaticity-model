c91917b1ed21ef4760b22a58ecf955bcc8ba112c
Andrew Tran <andrew.tran345@gmail.com>
2017-03-22 01:05:58 -0700

    Changed particle swarm opt; removed comment

--- particleSwarmOpt.m
+++ particleSwarmOpt.m
@@ -6,6 +6,6 @@ nvars = 2;
 lb = [0 + eps,  200];
 ub = [   1e-6, 1000];
 % Declare optimization options
-options = optimoptions(@fmincon, 'UseParallel', true, 'Display', 'iter');
+options = optimoptions(@particleswarm, 'HybridFcn', @fmincon, 'UseParallel', true, 'Display', 'iter');
 % Call particleswarm function (requires Matlab 2014 or higher)
 [x,fval,exitflag,output] = particleswarm(fun, nvars, lb, ub, options);
