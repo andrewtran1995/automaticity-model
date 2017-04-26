112e5c04d1b9c0018081906c4077522bdb050e88
Andrew Tran <andrew.tran345@gmail.com>
2017-04-26 15:31:54 -0700

    Started line iteration logic

--- pythonScripts/create_stim_vectors.py
+++ pythonScripts/create_stim_vectors.py
@@ -10,8 +10,11 @@ import re
 SOURCE_DIR = 'D:\\Users\\Andrew\\Google Drive\\UCSB\\Computational Cognitive Neuroscience Research\\fMRI Behavioral Data Folder\\rb_automaticity_no_study_folders'
 TARGET_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fmri_data')
 
-# Regex
 
+def non_blank_lines(f):
+    for line in f:
+        if line is not None:
+            yield line
 
 if __name__ == '__main__':
     for dirname in (filepath for filepath in os.listdir(SOURCE_DIR) if re.fullmatch('Sub[0-9]+_Ses[0-9]+', filepath) is not None):
@@ -28,4 +31,9 @@ if __name__ == '__main__':
                     log_file = filename
         assert (expertise_data, log_file) is not (None, None)
         with open(os.path.join(SOURCE_DIR, dirname, log_file), 'r') as f:
-            print("all is well")
+            for line in non_blank_lines(f):
+                print(line)
+                str_arr = line.split()
+                if str_arr[0] == 'TR':
+                    print(str_arr[-1])
+
