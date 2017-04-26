40c83e359d82dab0c7c9d504cf1c5572f4ab8005
Andrew Tran <andrew.tran345@gmail.com>
2017-04-26 02:05:08 -0700

    Can iterate through necessary files now

--- pythonScripts/create_stim_vectors.py
+++ pythonScripts/create_stim_vectors.py
@@ -3,6 +3,7 @@ Iterate through different folders in order to create box car vectors.
 """
 import numpy as np
 import os
+import re
 
 
 # Directories
@@ -13,5 +14,18 @@ TARGET_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fmri_data
 
 
 if __name__ == '__main__':
-    for dirname in os.listdir(SOURCE_DIR):
-        print(dirname)
+    for dirname in (filepath for filepath in os.listdir(SOURCE_DIR) if re.fullmatch('Sub[0-9]+_Ses[0-9]+', filepath) is not None):
+        [subject, session] = re.findall('[0-9]+', dirname)
+        print(subject + ' ' + session)
+        expertise_data = None
+        log_file = None
+        for filename in os.listdir(os.path.join(SOURCE_DIR, dirname)):
+            match = re.fullmatch('.*(data\.dat|log\.dat)', filename)
+            if match is not None:
+                if re.fullmatch('.*data\.dat', match.group()) is not None:
+                    expertise_data = np.loadtxt(os.path.join(SOURCE_DIR, dirname, filename))
+                else:
+                    log_file = filename
+        assert (expertise_data, log_file) is not (None, None)
+        with open(os.path.join(SOURCE_DIR, dirname, log_file), 'r') as f:
+            print("all is well")
