b56fc96f4ed29618cd8479d6e10078ebffa58d5d
Andrew Tran <andrew.tran345@gmail.com>
2017-05-29 22:19:07 -0700

    Changed back to 2000
    Former-commit-id: dd421f67a4c63483c7a30da9dcdcc6334561b0eb [formerly cb380d4c62a2ec70720f6005d0ea3c82457212a6]
    Former-commit-id: 0d3701dfc20d0e9891657238381621a14b946f41

--- fmri/replaceNeuronOutput.m
+++ fmri/replaceNeuronOutput.m
@@ -1,10 +1,12 @@
-function [ outputVectorMap ] = replaceNeuronOutput(outputVectors, stimVectorMap)
+function [ outputVectorMap ] = replaceNeuronOutput(outputVectors, latency, stimVectorMap)
 %REPLACENEURONOUTPUT Replace stimulus vectors w/ summed neural output
 %   Replace stimulus vectors (binary vector) with summed PMC A and PMC B 
 %   output from model, such that trials are aligned between data
     FMRI_META = struct('NUM_TRIALS', 11520, ...
                        'SES_1',      1:480, 'SES_4',    1681:2160, ...
                        'SES_10', 5161:5640, 'SES_20', 11041:11520);
+    % Assuming there will be no more than 2000 TRs for a session
+    MAX_OUTPUT_LENGTH = 4000000;
     stimKeys = keys(stimVectorMap);
     outputVectorMap = containers.Map('KeyType', 'double', 'ValueType', 'any');
     for i = 1:length(stimVectorMap)
@@ -20,10 +22,10 @@ function [ outputVectorMap ] = replaceNeuronOutput(outputVectors, stimVectorMap)
             % Find the length of each run by taking the difference
             % between elements, appending one more for length of last run
             len = [diff(idx), length(arr) - idx(end) + 1];
-            % Only look at runs of 1
-            stimulusOn = arr(idx) == 1;
-            idx = idx(stimulusOn);
-            len = len(stimulusOn);
+            % Only look at runs of 0
+%             stimulusOff = arr(idx) == 0;
+%             idx = idx(stimulusOff);
+%             len = len(stimulusOff);
             % Determine session
             switch j
                 case 1
@@ -35,15 +37,27 @@ function [ outputVectorMap ] = replaceNeuronOutput(outputVectors, stimVectorMap)
                 case 20
                     session = FMRI_META.SES_20;
             end
-            % Replace runs with output
+            % Fill in runs of no stimulus (0s), and replace runs of
+            % stimulus (1s) with outputVectors' data
+            % Use value of -1 to indicate uninitialized value
             output = outputVectors(session,:);
-            newSession = zeros(length(arr),1);
+            newSession = ones(MAX_OUTPUT_LENGTH,1)*-1;
+            lastIdx = 1;
+            sessionNum = 1;
             for k = 1:length(idx)
-                % Discrepancy where outputVectors only has output for
-                % 1000 time units max, whereas stimVectors has TRs of
-                % 2000 time units
-                newSession(idx(k):idx(k)+len(k)-1) = output(k,1:len(k));
+                if arr(idx(k)) == 0
+                    newSession(lastIdx:lastIdx+len(k)) = 0;
+                    lastIdx = lastIdx + len(k);
+                else
+                    newSession(lastIdx:lastIdx+latency(sessionNum)) = output(sessionNum, latency(sessionNum));
+                    lastIdx = lastIdx + latency(sessionNum);
+                    sessionNum = sessionNum + 1;
+                end
             end
+            % Verify newSession's buffer did not fill up, then trim it
+            assert(any(newSession == -1));
+            newSession(newSession == -1) = [];
+            % Assign newSession to appropriate cell of newSubject
             newSubject{j} = newSession;
         end
         outputVectorMap(stimKeys{i}) = newSubject;
