678b79b53b622c329a9bacc75ce3a37b3ab5855e
Andrew Tran <andrew.tran345@gmail.com>
2017-07-10 17:17:02 -0700

    Clarified maps to distinguish between 1D and Disj; also renamed vectors to stimulus to avoid ambiguity
    Former-commit-id: aac69e65a989fec1d32adfcdf0c89b91b6fb896e [formerly defd3993edb16007b39820164f9d4489fae6b4ef] [formerly 936b7c2179633aeb699f42544dd4f57d961c5fe1 [formerly c749748e272d34dfc46b08deaa79c258cf780564]]
    Former-commit-id: bfebe761d37014f2c5b08bb9e59d6d611e7bc419 [formerly a437bdccc886aa4990736455c696098d2f45f134]
    Former-commit-id: a0a2041e193e4141e47971daef5e88b9ac171271

--- fmri/getTRBoldScript.m
+++ fmri/getTRBoldScript.m
@@ -1,11 +1,11 @@
 LOADED_PMC = load('pmcAlphaVectors_17_6_2.mat');
-LOADED_STIM = load('vectors/stimVectors.mat');
+LOADED_STIM = load('stimulus/stimulusVectorsMap1D.mat');
 
 % modelAlphaVectorMap is ridiculously huge (120+ MB), so don't bother
 % saving it to a .mat file, but use it to calculate TRBoldMap instead
 modelAlphaVectorMap = replaceNeuronOutput(LOADED_PMC.PMC_ALPHA, ...
                                           LOADED_PMC.LATENCY, ...
-                                          LOADED_STIM.stimVectors);
+                                          LOADED_STIM.stimulusVectorsMap);
 
 % TRBoldMap is much more suitable to save to a .mat file
 TRBoldMap = generateTRBoldForMap(modelAlphaVectorMap);
