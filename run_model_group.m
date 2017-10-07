9975ed914e8f7746827f218115e34457a047475b
Andrew Tran <andrew.tran345@gmail.com>
2017-10-07 11:46:22 -0700

    Fixed bugs in run_model_group and added parfor loop
    Former-commit-id: c27ea8b5f270fe01810179a6816bdc20f2af09a2 [formerly 3c723159ed9b033441ca620acb7453dbdad0ee4e]
    Former-commit-id: fbe50b4671bd74ad1e2c68e417c01f6377c4c3f5

--- run_model_group.m
+++ run_model_group.m
@@ -1,23 +1,24 @@
 RUNS = 8;
-PMC_BOLD_S1  = zeros(1,RUNS);
-PMC_BOLD_S4  = zeros(1,RUNS);
-PMC_BOLD_S10 = zeros(1,RUNS);
-PMC_BOLD_S20 = zeros(1,RUNS);
-Accuracy_S1  = zeros(1,RUNS);
-Accuracy_S4  = zeros(1,RUNS);
-Accuracy_S10 = zeros(1,RUNS);
-Accuracy_S20 = zeros(1,RUNS);
+PMC_BOLD_S1  = zeros(RUNS,1);
+PMC_BOLD_S4  = zeros(RUNS,1);
+PMC_BOLD_S10 = zeros(RUNS,1);
+PMC_BOLD_S20 = zeros(RUNS,1);
+Accuracy_S1  = zeros(RUNS,1);
+Accuracy_S4  = zeros(RUNS,1);
+Accuracy_S10 = zeros(RUNS,1);
+Accuracy_S20 = zeros(RUNS,1);
 
 loaded_value = load('fmri/particleSwarmX_17_6_2.mat');
 arg_vector = loaded_value.x;
 
-for i=1:RUNS
+gcp;
+parfor i=1:RUNS
     [r_x_vals, r_y_vals] = createFMRIInput(11520);
     visualinput = [r_x_vals, r_y_vals];
     optional_parms = struct('FMRI_META_GROUP_RUN', 1, ...
                             'VIS_INPUT_FROM_PARM', 1, ...
                             'visualinput', visualinput);
-    [unused_val,results] = automaticityModelFast(arg_vector, optional_parms);
+    [unused_val,results] = automaticityModelFast_mex(arg_vector, optional_parms);
     PMC_BOLD_S1(i)  = results(1);
     PMC_BOLD_S4(i)  = results(2);
     PMC_BOLD_S10(i) = results(3);
@@ -27,3 +28,11 @@ for i=1:RUNS
     Accuracy_S10(i) = results(7);
     Accuracy_S20(i) = results(8);
 end
+delete(gcp('nocreate'));
+
+PMC_corr = [corr(PMC_BOLD_S1,Accuracy_S1), corr(PMC_BOLD_S4,Accuracy_S4), corr(PMC_BOLD_S10,Accuracy_S10), corr(PMC_BOLD_S20,Accuracy_S20)];
+
+figure;
+plot(PMC_corr);
+title('PMC Correlations (Average Bold vs. Accuracy)');
+axis([0 4 -1.1 1.1]);
