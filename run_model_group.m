df6e5faba49e8249d6d1c8743ec952be7c0f4a72
Andrew Tran <andrew.tran345@gmail.com>
2017-11-30 01:12:19 -0800

    Added automatic path addition for function, and graphing of correlation
    Former-commit-id: 89fbf4d347dece1f7d00311042ef725796af7dbb [formerly da8d35cf8eb3706329f5d97111011afdc121846a]
    Former-commit-id: b51e02493fa9b25efb991c1a4abcde2392742f6f

--- run_model_group.m
+++ run_model_group.m
@@ -1,14 +1,17 @@
+% Add createFMRIInput function to path
+addpath('matGenerationScripts');
+
 % Set number of subjects and result matrix
 GROUP_SIZE = 12;
-results = zeros(5,4,GROUP_SIZE);
+results = zeros(6,4,GROUP_SIZE);
 
 % Get parameters for automaticityModel
-loaded = load('fmri/particleswarm_target_17_11_12.mat');
+loaded = load('fmri/particle_swarm_results_11_22.mat');
 arg_vector = loaded.x;
 CONFIG = 'FMRI';
 
 % Create parallel pool
-gcp;
+poolobj = gcp;
 parfor i=1:GROUP_SIZE
     arg_struct = argvectortostruct(arg_vector, CONFIG);
     [r_x_vals, r_y_vals] = createFMRIInput(11520);
@@ -23,7 +26,27 @@ delete(gcp('nocreate'));
 
 % Get correlation information from results
 permuted_results = permute(results(:,:,:), [3 2 1]);
-corr_mat = zeros(4,4);
-for i=1:4
-    corr_mat(i,:) = corrneuron(permuted_results(:,:,i), permuted_results(:,:,5));
+corr_mat = zeros(5,4);
+for i=1:5
+    corr_mat(i,:) = corrneuron(permuted_results(:,:,i), permuted_results(:,:,6));
 end
+
+% Plot correlation data
+figure;
+rows = 2; columns = 3;
+
+subplot(rows,columns,1);
+plot(corr_mat(1,:));
+title('PFC');
+subplot(rows,columns,2);
+plot(corr_mat(2,:));
+title('CN');
+subplot(rows,columns,3);
+plot(corr_mat(3,:));
+title('GP');
+subplot(rows,columns,4);
+plot(corr_mat(4,:));
+title('MDN');
+subplot(rows,columns,5);
+plot(corr_mat(5,:));
+title('PMC');
