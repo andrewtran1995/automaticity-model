bcb997bd29f0ded4596481094d893a5a36ac96d4
Andrew Tran <andrew.tran345@gmail.com>
2018-01-16 21:55:39 -0800

    Fixed bug with PMC_*.W_OUT and added it to optimization; changed heb random variable distribution
    Former-commit-id: af54a507d83a5e0afc483f7f8a027442e4497ef9 [formerly 551871393e45f93e0e716f8027375471309f8f6e]
    Former-commit-id: 5411ed3c0f9fad42d7bd23e9a7b68f3798bb5e49

--- argvectortostruct.m
+++ argvectortostruct.m
@@ -12,12 +12,14 @@ function [ arg_struct ] = argvectortostruct( arg_vector, configuration )
     params.NMDA            = arg_vector(6);
     params.AMPA            = arg_vector(7);
     params.W_MAX           = arg_vector(8);
+    params.PMC_A_W_OUT     = arg_vector(9);
+    params.PMC_B_W_OUT     = arg_vector(10);
 
     % Get normal distribution parameters for certain model parms
-    rnd_1                  = arg_vector(9);
-    rnd_2                  = arg_vector(10);
-    rnd_3                  = arg_vector(11);
-    rnd_4                  = arg_vector(12);
+    rnd_1                  = arg_vector(10);
+    rnd_2                  = arg_vector(11);
+    rnd_3                  = arg_vector(12);
+    rnd_4                  = arg_vector(13);
     params.HEB_CONSTS      = autohebrnd(rnd_1, rnd_2);
     params.PFC_A_W_OUT_MDN = autowmrnd(rnd_3, rnd_4);
     params.PFC_B_W_OUT_MDN = autowmrnd(rnd_3, rnd_4);
@@ -26,10 +28,10 @@ function [ arg_struct ] = argvectortostruct( arg_vector, configuration )
     params.MDN_B_W_OUT     = autowmrnd(rnd_3, rnd_4);
     
     % Set static values for COVIS
-    params.COVIS_DELTA_C   = arg_vector(13);
-    params.COVIS_DELTA_E   = arg_vector(14);
-    params.COVIS_PERSEV    = arg_vector(15);
-    params.COVIS_LAMBDA    = arg_vector(16);
+    params.COVIS_DELTA_C   = arg_vector(14);
+    params.COVIS_DELTA_E   = arg_vector(15);
+    params.COVIS_PERSEV    = arg_vector(16);
+    params.COVIS_LAMBDA    = arg_vector(17);
     
     arg_struct = params;
 end
