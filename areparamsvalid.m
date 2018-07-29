38e6bb82392a45c756e4e546d55b9cdb4d0629a5
Andrew Tran <andrew.tran345@gmail.com>
2018-07-29 12:47:38 -0700

    Implemented naive implementation of areparamsvalid
    Former-commit-id: fd55f16cd88dd9b0e6be2980816ab7881f3f32f2 [formerly 3a028330a0643a604ef9aacc47b6eda7a8bdcf95]
    Former-commit-id: ac4a177c7a00786d816b2cee9ec4260430814a98

--- areparamsvalid.m
+++ areparamsvalid.m
@@ -1,6 +1,6 @@
 function [ params_valid ] = areparamsvalid( param_struct )
 %VALIDATEPARAMS Validates that params are valid biologically/functionally
-    params_valid = 1;
+    params_valid = all(structfun(@(x) x >= 0, param_struct));
     return;
 end
 
