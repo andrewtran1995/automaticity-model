f039b63326107feb4a691cda5a8413952a857e40
andrewtran1995 <andrew.tran345@gmail.com>
2020-09-01 13:41:40 -0700

    Updated Hebbian values

--- internal/config/params/getconstants.m
+++ internal/config/params/getconstants.m
@@ -2,7 +2,7 @@ function [params] = getconstants()
 %GETCONSTANTS Get configuration-agnostic constants.
     % Initialize configuration-agnostic parameters used in optimization
     agn_names = {'HEB_CONSTS';'NMDA';'AMPA';'W_MAX';'NOISE_PFC';'NOISE_PMC';'NOISE_MC';'PMC_A_W_OUT';'PMC_B_W_OUT'};
-    agn_vals  = {       1e-8;  450;    200;     10;        2.4;          2;         5;            1;            1};
+    agn_vals  = {       1e-8;  450;    300;     10;        2.4;          2;         5;            1;            1};
     % FROST Params
     frost_names = {'PFC_A_W_OUT_MDN';'PFC_B_W_OUT_MDN';'DRIV_PFC_W_OUT';'MDN_A_W_OUT';'MDN_B_W_OUT'};
     frost_vals  = {                5;                5;               5;            5;            5};
